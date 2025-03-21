4cf07d8b94729bfbe7e1b6f5b095a2e831f10594
Sercan Yüksel <sercanyksl.php@gmail.com>
2025-03-21 12:03:20 +0300

    fix

--- resources/views/livewire/chat.blade.php
+++ resources/views/livewire/chat.blade.php
@@ -5,7 +5,7 @@
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-refresh-cw"><path d="M3 12a9 9 0 0 1 9-9 9.75 9.75 0 0 1 6.74 2.74L21 8"/><path d="M21 3v5h-5"/><path d="M21 12a9 9 0 0 1-9 9 9.75 9.75 0 0 1-6.74-2.74L3 16"/><path d="M8 16H3v5"/></svg> Yenile
         </button>
     </div>
-
+    
     <div x-data="{ messages: [], message: '', scrollToBottom() { this.$nextTick(() => { this.$refs.messageList.scrollTop = this.$refs.messageList.scrollHeight }) } }">
         <div x-ref="messageList" x-init="scrollToBottom" class="max-w-7xl mx-auto bg-stone-200/50 rounded-lg h-[500px] border border-gray-300 shadow-lg mt-3 p-2 overflow-auto">
             
